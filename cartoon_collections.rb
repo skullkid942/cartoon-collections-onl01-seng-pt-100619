0d7b7ce789114a17f521db96b9e0dac52fce30e1
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-17 17:41:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,4 +19,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if 
+    array.include?("cheddar", "gouda", "camembert")
+    return 
 end
