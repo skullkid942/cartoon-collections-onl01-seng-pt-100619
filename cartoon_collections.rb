9aa73f22f821c9404001bdf0fc7188dae0bd3e05
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-17 17:34:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(array)
-  array.any? {|call| call.
+  array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
